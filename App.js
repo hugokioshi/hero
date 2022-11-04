a4ece6e04e7a6e811c1b974e68edf15aeeb63d9f
Hugo Kioshi <49104526+hugokioshi@users.noreply.github.com>
2022-11-04 13:17:41 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import  {NavigationContainer} from '@react-navigation/native';
+
+import Route from './src/navigations/Route';
+
+import UserProvider from './src/contexts/UserContext';
+
+
+const App = () => {
+  return(
+    <UserProvider>
+      <NavigationContainer>
+        <Route /> 
+      </NavigationContainer>
+    </UserProvider>
+  );
+};
+  
+export default App;
